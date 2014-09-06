97d233a1d6c05a956de1c24801c30f9ccbf85cab
Adrian Göransson <adriangoransson@gmail.com>
2014-09-06 13:09:08 +0200

    Updated zprezto installer

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 dotfiles=$HOME/.dotfiles
 
 # Install prezto
@@ -6,8 +8,10 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 # Prezto files
 ln -s "$HOME/.zprezto/runcoms/zprofile" "$HOME/.zprofile"
 ln -s "$HOME/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
+ln -s "$HOME/.zprezto/runcoms/zlogin" "$HOME/.zlogin"
 
 # Personal configuration
 ln -s "$dotfiles/zsh/.zpreztorc" "$HOME/.zpreztorc"
 ln -s "$dotfiles/zsh/.zshrc" "$HOME/.zshrc"
 
+chsh -s /bin/zsh
