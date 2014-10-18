7236dc800366b42aacd2b6a41dc554e0aff9e2bd
Adrian Göransson <adriangoransson@gmail.com>
2014-10-19 01:56:39 +0200

    Add symlink for sublime binary

--- sublime/install.sh
+++ sublime/install.sh
@@ -4,4 +4,6 @@ dotfiles=$(dirname $0:A)
 
 if [[ `uname` == 'Darwin' ]]; then
     ln -s "$dotfiles/Packages/User" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+    mkdir $HOME/bin
+    ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "$HOME/bin/subl"
 fi
