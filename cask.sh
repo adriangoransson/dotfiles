ef4e8e51ba9a1a4fc60f49872153a2411f268472
Adrian Göransson <adriangoransson@gmail.com>
2014-10-19 12:56:58 +0200

    Replaced brew and caskfile with shell scripts

---
+++ cask.sh
@@ -0,0 +1,13 @@
+APPS="firefox-beta dropbox google-drive fantastical alfred flux sublime-text3 mplayerx steam skype spotify google-chrome"
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+brew install caskroom/cask/brew-cask
+
+brew tap caskroom/versions
+
+for APP in $APPS; do
+    brew cask install $APP
+done
+
+brew cask alfred link
