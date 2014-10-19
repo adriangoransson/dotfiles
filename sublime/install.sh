df33bc334c028b5f5dfbb0192523a5317bf4776c
Adrian Göransson <adriangoransson@gmail.com>
2014-10-19 12:57:36 +0200

    Sublime fix.

--- sublime/install.sh
+++ sublime/install.sh
@@ -3,7 +3,5 @@
 dotfiles=$(dirname $0:A)
 
 if [[ `uname` == 'Darwin' ]]; then
-    ln -s "$dotfiles/Packages/User" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-    mkdir $HOME/bin
-    ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "$HOME/bin/subl"
+    ln -s "$dotfiles/Packages/User" "$HOME/Library/Application Support/Sublime Text 3/Packages"
 fi
