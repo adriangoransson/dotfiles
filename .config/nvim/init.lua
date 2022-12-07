fccd336535e5db6fe6acb6dafebfea6ad150137b
Adrian Göransson <adriangoransson@gmail.com>
2022-12-07 11:47:15 +0100

    nvim: fix Treesitter update command

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -92,7 +92,7 @@ require('packer').startup(function(use)
   use {
     {
       'nvim-treesitter/nvim-treesitter',
-      run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+      run = function() require('nvim-treesitter.install').update({ with_sync = true })() end,
       config = function()
         require('nvim-treesitter.configs').setup({
           ensure_installed = 'all',
