0ab29db85b7dc605078a3f8c846b5f5ae4f7fc1f
Adrian Göransson <git@adrian.fastmail.in>
2024-05-03 23:58:44 +0200

    nvim: Set module for treesitter in Lazy

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
     opts = {
       ensure_installed = {
         'awk',
@@ -54,7 +55,7 @@ return {
 
         disable = function(_, buf)
           local max_filesize = 100 *
-          1024                            -- 100 KB
+              1024 -- 100 KB
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
             return true
