2c77ea170aca8826b7a9e6ba91b45a393db77928
Adrian Göransson <git@adrian.fastmail.in>
2024-05-05 21:43:56 +0200

    nvim: Add friendly-snippets (and luasnip build step)

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -11,6 +11,22 @@ return {
 	},
 
 	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+		},
+
+		event = "InsertEnter",
+
+		build = "make install_jsregexp",
+	},
+
+	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			"L3MON4D3/LuaSnip",
