3a32773fcb9f8c43bf1d7ac75b59907b4a297296
Adrian Göransson <git@adrian.fastmail.in>
2024-08-08 10:01:10 +0200

    nvim: Update awk and jq TS injections for shell

--- .config/nvim/after/queries/bash/injections.scm
+++ .config/nvim/after/queries/bash/injections.scm
@@ -3,11 +3,15 @@
 (command
   name: (command_name) @_command
   (#match? @_command "awk")
-  argument: (raw_string) @awk
+  argument: (raw_string) @injection.content
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "awk")
 )
 
 (command
   name: (command_name) @_command
   (#match? @_command "jq")
-  argument: (raw_string) @jq
+  argument: (raw_string) @injection.content
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "jq")
 )
