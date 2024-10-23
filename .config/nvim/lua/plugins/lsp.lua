98bc79370349d742ba24f22e84c4f8bad340ae3c
Adrian Göransson <git@adrian.fastmail.in>
2024-10-23 23:36:27 +0200

    nvim: Set up LSP attach via mason-lspconfig

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,7 +13,87 @@ return {
 			},
 			{
 				"williamboman/mason-lspconfig.nvim",
-				opts = { automatic_installation = true },
+				config = function()
+					local c = require("mason-lspconfig")
+
+					c.setup({
+						ensure_installed = {
+							"cssls",
+							"gopls",
+							"html",
+							"rust_analyzer",
+							"svelte",
+							"ts_ls",
+						},
+					})
+
+					c.setup_handlers({
+						-- First entry (without a key) is the default handler.
+						function(server_name) -- default handler (optional)
+							require("lspconfig")[server_name].setup({})
+						end,
+
+						["gopls"] = function()
+							require("lspconfig")["gopls"].setup({
+								settings = {
+									gopls = {
+										staticcheck = true,
+										analyses = {
+											unusedwrite = true,
+											composites = false,
+										},
+										hints = {
+											assignVariableTypes = true,
+											compositeLiteralFields = true,
+											compositeLiteralTypes = true,
+											constantValues = true,
+											parameterNames = true,
+											rangeVariableTypes = true,
+										},
+									},
+								},
+							})
+						end,
+
+						["lua_ls"] = function()
+							require("lspconfig")["lua_ls"].setup({
+								settings = {
+									Lua = {
+										-- From https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+										-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+										runtime = { version = "LuaJIT" },
+										-- Get the language server to recognize the `vim` global
+										diagnostics = { globals = { "vim" } },
+										-- Make the server aware of Neovim runtime files
+										workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+										-- Do not send telemetry data containing a randomized but unique identifier
+										telemetry = { enable = false },
+
+										-- Trailing commas.
+										format = {
+											defaultConfig = {
+												quote_style = "single",
+												trailing_table_separator = "smart",
+											},
+										},
+									},
+								},
+							})
+						end,
+
+						["rust_analyzer"] = function()
+							require("lspconfig")["rust_analyzer"].setup({
+								settings = {
+									["rust-analyzer"] = {
+										check = {
+											command = "clippy",
+										},
+									},
+								},
+							})
+						end,
+					})
+				end,
 			},
 			{
 				"folke/lazydev.nvim",
@@ -28,93 +108,40 @@ return {
 		},
 
 		config = function()
-			local lspconfig = require("lspconfig")
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					-- TODO: check if telescope is available first.
+					local telescope = require("telescope.builtin")
 
-			local on_attach = function(_, bufnr)
-				-- TODO: check if telescope is available first.
-				local telescope = require("telescope.builtin")
+					local opts = { noremap = true, silent = true, buffer = args.buf }
+					local map = function(mode, lhs, rhs)
+						vim.keymap.set(mode, lhs, rhs, opts)
+					end
 
-				local opts = { noremap = true, silent = true, buffer = bufnr }
-				local map = function(mode, lhs, rhs)
-					vim.keymap.set(mode, lhs, rhs, opts)
-				end
+					vim.api.nvim_create_user_command("InlayHintsToggle", function()
+						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
+					end, {})
 
-				map("n", "gD", vim.lsp.buf.declaration)
-				map("n", "gd", vim.lsp.buf.definition)
-				map("n", "K", vim.lsp.buf.hover)
-				map("n", "gi", telescope.lsp_implementations)
-				map("i", "<C-k>", vim.lsp.buf.signature_help)
-				map("n", "<leader>D", vim.lsp.buf.type_definition)
-				map("n", "<leader>rn", vim.lsp.buf.rename)
-				map("n", "gr", telescope.lsp_references)
-				map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
-				map("n", "<leader>e", vim.diagnostic.open_float)
-				map("n", "[d", vim.diagnostic.goto_prev)
-				map("n", "]d", vim.diagnostic.goto_next)
-				map("n", "<leader>q", vim.diagnostic.setloclist)
-				map("n", "<leader>wq", vim.diagnostic.setqflist)
-				map("n", "<leader>so", telescope.lsp_document_symbols)
-				map("n", "<leader>wso", telescope.lsp_dynamic_workspace_symbols)
+					vim.lsp.inlay_hint.enable(true)
 
-				vim.api.nvim_create_user_command("InlayHintsToggle", function()
-					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
-				end, {})
-
-				vim.lsp.inlay_hint.enable(true)
-			end
-
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-			local servers = {
-				gopls = {
-					gopls = {
-						staticcheck = true,
-						analyses = {
-							unusedwrite = true,
-							composites = false,
-						},
-					},
-				},
-				bashls = {},
-				clangd = {},
-				eslint = {},
-				volar = {},
-				tsserver = {},
-				pylsp = {},
-				html = {},
-				cssls = {},
-				lua_ls = {
-					Lua = {
-						-- From https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-						-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-						runtime = { version = "LuaJIT" },
-						-- Get the language server to recognize the `vim` global
-						diagnostics = { globals = { "vim" } },
-						-- Make the server aware of Neovim runtime files
-						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-						-- Do not send telemetry data containing a randomized but unique identifier
-						telemetry = { enable = false },
-
-						-- Trailing commas.
-						format = {
-							defaultConfig = {
-								quote_style = "single",
-								trailing_table_separator = "smart",
-							},
-						},
-					},
-				},
-				svelte = {},
-			}
-
-			for lsp, settings in pairs(servers) do
-				lspconfig[lsp].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-					settings = settings,
-				})
-			end
+					map("n", "gD", vim.lsp.buf.declaration)
+					map("n", "gd", vim.lsp.buf.definition)
+					map("n", "K", vim.lsp.buf.hover)
+					map("n", "gi", telescope.lsp_implementations)
+					map("i", "<C-k>", vim.lsp.buf.signature_help)
+					map("n", "<leader>D", vim.lsp.buf.type_definition)
+					map("n", "<leader>rn", vim.lsp.buf.rename)
+					map("n", "gr", telescope.lsp_references)
+					map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
+					map("n", "<leader>e", vim.diagnostic.open_float)
+					map("n", "[d", vim.diagnostic.goto_prev)
+					map("n", "]d", vim.diagnostic.goto_next)
+					map("n", "<leader>q", vim.diagnostic.setloclist)
+					map("n", "<leader>wq", vim.diagnostic.setqflist)
+					map("n", "<leader>so", telescope.lsp_document_symbols)
+					map("n", "<leader>wso", telescope.lsp_dynamic_workspace_symbols)
+				end,
+			})
 		end,
 	},
 }
