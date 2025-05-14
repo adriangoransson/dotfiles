7e9c671268cd9ad7f8cef66ee6b051a01312977e
Adrian Göransson <git@adrian.fastmail.in>
2025-05-14 20:50:39 +0200

    nvim: Include Neovim's own LSP capabilities in setup

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -65,14 +65,6 @@ return {
 				enabled = false,
 			},
 		},
-
-		config = function(_, opts)
-			require("blink-cmp").setup(opts)
-
-			vim.lsp.config("*", {
-				capabilities = require("blink.cmp").get_lsp_capabilities(),
-			})
-		end,
 	},
 
 	{
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,19 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+			-- Enable more capabilities from completion plugin if available.
+			local ok, blink = pcall(require, "blink.cmp")
+			if ok then
+				capabilities = blink.get_lsp_capabilities({}, true)
+			end
+
+			vim.lsp.config("*", {
+				capabilities = capabilities,
+			})
+		end,
 	},
 
 	{
