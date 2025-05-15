76d3dd8f598556844b9393924ac44ecd1be92d76
Adrian Göransson <git@adrian.fastmail.in>
2025-05-15 09:43:08 +0200

    nvim: Prefer vim.uv instead of vim.loop

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -55,8 +55,8 @@ return {
 
 				disable = function(_, buf)
 					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
+					local stats = (vim.uv or vim.loop).fs_stat(vim.api.nvim_buf_get_name(buf))
+					if stats and stats.size > max_filesize then
 						return true
 					end
 				end,
